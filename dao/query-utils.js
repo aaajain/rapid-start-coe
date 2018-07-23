87605fbd722ba81e2b60364a6849c0a30021898d
aaajain <aajain@chenoainc.com>
2018-07-23 20:43:16 +0530

    use role_admin

--- dao/query-utils.js
+++ dao/query-utils.js
@@ -7,7 +7,7 @@ var methods =
 		var conn = mongo.client;
 		const role_admin = conn.model('role_master');
 		const admin = new role_admin({role_name:"admin", permissions:['view','create','modify','delete']});
-		conn.collection("role_masters").find({}, { role: 'admin' }).toArray(function(err, result) {
+		conn.collection("role_masters").find({ role_name: 'admin' }).toArray(function(err, result) {
 	    	if (err) throw err;
 	    	else if(result && result.length > 0)
 	    	{
@@ -34,7 +34,7 @@ var methods =
 	createAdminUser: function(callback){
 		var conn = mongo.client;
 		const admin_user= conn.model('user_master');
-		conn.collection("role_masters").find({ role: 'admin' }).toArray(function(err, result) {
+		conn.collection("role_masters").find({ role_name: 'admin' }).toArray(function(err, result) {
 	    	if (err) throw err;
 	    	else if(result && result.length > 0)
 	    	{
