5001a1b4cb9238971dc29ef25d5c5c6cf2412327
aaajain <aajain@chenoainc.com>
2018-07-26 15:11:28 +0530

    added test for authorization

---
+++ test/mock-authorization.js
@@ -0,0 +1,20 @@
+process.env.NODE_ENV = 'test';
+var sinon = require('sinon');
+var queryUtils = require('../dao/query-utils.js');
+var AuthorizationHelper = require('../helper/authorization.js');
+var assert     =  require('assert')
+
+describe('test authorization', function () {
+	before(function () {
+		sinon.stub(queryUtils.methods,"checkUserPermissionForAction").yields(null,true);
+	});
+	it('test if user was authorized', function (done) {
+		 AuthorizationHelper.auth({'user':'testUser','action':'create'},function(err,result){
+		 	assert.equal(res.text,JSON.stringify(expected_result));
+		 })
+		 done();
+	});
+	after(function () {
+		queryUtils.methods.checkUserPermissionForAction.restore();
+	});
+});
