48488888040405c68eef8f7f505b339711a6d7c1
Ishan Parekh <IParekh@chenoainc.com>
2018-08-09 13:01:05 +0530

    modified query=utils to add bcrypt functionality for createAdminUser method

--- dao/query-utils.js
+++ dao/query-utils.js
@@ -1,6 +1,7 @@
 var mongo = require('../dao/mongo-connect.js');
 var logger = require('../config/logger.js').getLogger('query-utils');
 var constants = require('../util/Constants.js');
+var bcrypt = require('bcrypt');
 
 var roleMasterData = {};
 
@@ -36,6 +37,9 @@ var methods =
 	},
 	createAdminUser: function(callback){
 		var conn = mongo.client;
+		const dummyPassword = 'admin';
+		var salt = bcrypt.genSaltSync(constants.SALT_ROUNDS);
+		var hash = bcrypt.hashSync(dummyPassword, salt);
 		const admin_user= conn.model('user_master');
 		conn.collection("role_masters").find({ role_name: 'admin' }).toArray(function(err, result) {
 	    	if (err) throw err;
@@ -43,7 +47,7 @@ var methods =
 	    	{
 	    		var roleId = result[0]._id;
 	    		//const admin = new admin_user({ email: 'admin@admin.com',username:'admin',password:'test',role:roleId});
-	    		admin_user.findOneAndUpdate({username:'admin'},{$set: { email: 'admin@admin.com',username:'admin',password:'test',role:roleId}},{upsert: true, new: true, runValidators: true},function (err,doc) {
+	    		admin_user.findOneAndUpdate({username:'admin'},{$set: { email: 'admin@admin.com',username:'admin',password:hash,role:roleId}},{upsert: true, new: true, runValidators: true},function (err,doc) {
 					 if(err)
 					 {
 					 	logger.debug(err.stack);
